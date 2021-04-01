53b66b4df20e6e3bad3cb29647ffea0aca1a0dc7
say90688 <sdh180421@sdh.hs.kr>
2021-04-01 17:54:52 +0900

    mouse tracking (70%)

--- exercise/js/mouse_tracking/script.js
+++ exercise/js/mouse_tracking/script.js
@@ -1,8 +1,11 @@
 document.addEventListener('mousemove', function(e){
-    var position_x = e.clientX
-    var position_y = e.clientY
-
-    var eyes = document.getElementsByClassName(eyes)
-
-    console.log(eyes)
+    let left_eyes = document.querySelector('#left_eyes > div');
+    let right_eyes = document.querySelector('#right_eyes > div');
+    var position_x = e.clientX * 100 / window.innerWidth - 100;
+    var position_y = e.clientY * 100 / window.innerHeight - 100;
+    // var window_center = [document.body.clientWidth / 2, document.body.clientHeight / 2]
+    
+    left_eyes.style.transform = `translate(${position_x}%, ${position_y}%)`;
+    right_eyes.style.transform = `translate(${position_x}%, ${position_y}%)`;
+    console.log("position_x, position_y", position_x, position_y);
 });
